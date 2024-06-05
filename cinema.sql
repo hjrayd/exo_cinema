4957d75b0709c9be99a6b6bc5a8be552d7ff208b
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-06-05 10:48:56 +0200

    querries (not done)

---
+++ cinema.sql
@@ -0,0 +1,30 @@
+/*1-Informations d’un film (id_film) : titre, année, durée (au format HH:MM) et 
+réalisateur*/
+
+ /*2-Liste des films dont la durée excède 2h15 classés par durée (du + long au + court)*/
+
+ /*3-Liste des films d’un réalisateur (en précisant l’année de sortie)*/
+
+  /*4-Nombre de films par genre (classés dans l’ordre décroissant)*/
+
+  /*5-Nombre de films par réalisateur (classés dans l’ordre décroissant)*/
+
+  /*6-Casting d’un film en particulier (id_film) : nom, prénom des acteurs + sexe*/
+
+  /*7-Films tournés par un acteur en particulier (id_acteur) avec leur rôle et l’année de 
+sortie (du film le plus récent au plus ancien)*/
+
+/*8-Liste des personnes qui sont à la fois acteurs et réalisateurs*:
+
+/*9-Liste des films qui ont moins de 5 ans (classés du plus récent au plus ancien)*/
+
+/*10-Nombre d’hommes et de femmes parmi les acteurs*/
+
+/*11-Liste des acteurs ayant plus de 50 ans (âge révolu et non révolu)*/
+
+/*12-Acteurs ayant joué dans 3 films ou plus*/
+
+
+
+
+
