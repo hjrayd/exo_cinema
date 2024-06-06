f1180bdf44a38d6d9c26d062a6fe6fc4f881b5e5
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-06-06 14:09:34 +0200

    Querry 5

--- cinema.sql
+++ cinema.sql
@@ -1,12 +1,12 @@
 /*1-Informations d’un film (id_film) : titre, année, durée (au format HH:MM) et 
 réalisateur*/
-SELECT titre, date_sortie ,SEC_TO_TIME(duree*60) AS duree_heure, nom_personne
+SELECT film.titre, film.date_sortie ,SEC_TO_TIME(duree*60) AS duree_heure, personne.nom_personne
 FROM film
 INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur
 INNER JOIN personne ON realisateur.id_personne = personne.id_personne;
 
  /*2-Liste des films dont la durée excède 2h15 classés par durée (du + long au + court)*/
-SELECT titre, SEC_TO_TIME(duree*60) AS duree_heure 
+SELECT film.titre, SEC_TO_TIME(duree*60) AS duree_heure 
 FROM film
 WHERE duree > 135
 ORDER BY duree DESC;
@@ -17,9 +17,18 @@ ORDER BY duree DESC;
     INNER JOIN personne ON realisateur.id_personne = personne.id_personne
     WHERE personne.id_personne = 6;
   /*4-Nombre de films par genre (classés dans l’ordre décroissant)*/
-
+SELECT genre.nom_genre, COUNT(id_film)
+    FROM appartient
+    INNER JOIN genre ON appartient.id_genre = genre.id_genre
+    GROUP BY appartient.id_genre
+    ORDER BY appartient.id_genre DESC;
   /*5-Nombre de films par réalisateur (classés dans l’ordre décroissant)*/
-
+SELECT nom_personne, prenom_personne, COUNT(id_film) AS nb_film
+FROM film
+INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur
+INNER JOIN personne ON realisateur.id_personne = personne.id_personne
+GROUP BY film.id_realisateur
+ORDER BY nb_film DESC;
   /*6-Casting d’un film en particulier (id_film) : nom, prénom des acteurs + sexe*/
 
   /*7-Films tournés par un acteur en particulier (id_acteur) avec leur rôle et l’année de 
