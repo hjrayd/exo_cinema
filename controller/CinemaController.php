16e4b67402b93de42502c958087ef9db26475982
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-06-07 11:27:43 +0200

    listFilms + listActeurs

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -11,7 +11,20 @@
             FROM film
             ");
 
-            require "viex/listFilms.php";
+            require "view/listFilms.php";
+        }
+
+        public function listActeurs() {
+            $pdo = Connect::seConnecter();
+            $requete = $pdo->query("
+           SELECT nom_personne, prenom_personne
+            FROM personne
+            INNER JOIN acteur ON personne.id_personne = acteur.id_personne;
+            ");
+
+            require "view/lisActeurs.php";
         }
     }
 ?>
+
+
