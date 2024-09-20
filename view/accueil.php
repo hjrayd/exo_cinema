e1d70f35c19ae6748ee71121069f14db27601929
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-09-20 14:27:45 +0200

    beginning of css on homepage

--- view/accueil.php
+++ view/accueil.php
@@ -9,27 +9,25 @@ ob_start();
       <div id="wrapper">
          <div class="titre">
             <?php
-
             foreach($requete->fetchAll() as $aLaffiche) { ?>
-            <a href="index.php?action=detailFilm&id=<?= $aLaffiche["id_film"] ?>"><?= $aLaffiche["titre"] ?></a>
-            <img src="<?= $aLaffiche["afficheFilm"]?>" alt="Affiche du Film">
-            <p> <?= $aLaffiche["note"] ?>
-
-
-            <?php } ?>
-
-            <!--Mettre image en dessous du titre de chaque film-->
+            <div class="test">
+               <a href="index.php?action=detailFilm&id=<?= $aLaffiche["id_film"] ?>"><?= $aLaffiche["titre"] ?></a>
+               <img src="<?= $aLaffiche["afficheFilm"]?>" alt="Affiche du Film">
+               <p> <?= $aLaffiche["note"] ?>/5 </p>
+            </div>
+               <?php } ?>
          </div>
+
          <h2> Nouveautés </h2>
 
             <div class="nouveautes">
                <?php
                foreach($requetee->fetchAll() as $nouveaute) { ?>
-               <a href="index.php?action=detailFilm&id=<?= $nouveaute["id_film"] ?>"><?= $nouveaute["titre"] ?></a>
-               <img src="<?= $nouveaute["afficheFilm"]?>" alt="Affiche du Film">
-               
-      
-               <?php } ?>
+               <div class="nouveaute">
+                  <a href="index.php?action=detailFilm&id=<?= $nouveaute["id_film"] ?>"><?= $nouveaute["titre"] ?></a>
+                  <img src="<?= $nouveaute["afficheFilm"]?>" alt="Affiche du Film">
+                  <?php } ?>
+               </div>
             </div>
       </div>
 
